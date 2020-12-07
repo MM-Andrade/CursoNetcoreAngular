09f99ceaa3456ba1400d99e5d3ebb22fad74ff58
Marcelo Andrade <marcelo.m.andrade@outlook.com>
2020-12-07 20:37:02 -0300

    Commit inicial

---
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProAgil.WebAPI.Data;
+using ProAgil.WebAPI.Model;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ValuesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _dataContext.Eventos.ToListAsync();
+                return Ok(results);     
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro no banco de dados");
+            }
+           
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+                  try
+            {
+                var result =  await _dataContext.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                return Ok(result);     
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro no banco de dados");
+            }
+        }
+
+    }
+}
